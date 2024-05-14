35bf91d3fbb6e1b8db0657c8a03997dc3ef8847c
= <=>
2024-05-14 17:43:10 -0300

    main

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Text } from "react-native";
+import { StatusBar } from "react-native";
+
+
+export const Header = () => {
+
+    return (
+        <View>
+          <Text>GRANCOFFEE Preventivas!</Text>
+          <StatusBar style="auto" />
+        </View>
+      );
+    }
--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -1,10 +1,10 @@
 import { View, Text, StyleSheet } from "react-native"
-import { Cabecalho } from "../../componentes/Cabeçalho/index.js";
+import { Header } from "../../components/Header/index.tsx";
 
 export default function Home(){
     return (
-      <View>
-        <Cabecalho/>
+      <View style={{ flex:1, paddingVertical:30}}>
+        <Header/>
       </View>
     );
 }
