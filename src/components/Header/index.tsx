470b0582ef9ebb559c3dc13b22b6eac12d3f391c
= <=>
2024-05-14 17:58:11 -0300

    main

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,8 +5,17 @@ import { StatusBar } from "react-native";
 export const Header = () => {
 
     return (
-        <View>
-          <Text>GRANCOFFEE Preventivas!</Text>
+        <View style={{
+          alignItems:'center',
+          justifyContent:'center',
+          width: '100%',
+          height: 50,
+          backgroundColor:'black',
+        }}
+        >
+          <Text style={{
+            color:'white',
+          }}>GRANCOFFEE Preventivas!</Text>
           <StatusBar style="auto" />
         </View>
       );
