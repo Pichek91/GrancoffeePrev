68f0fc4f865ad682b24b6e0ffea55ac610028b53
= <=>
2024-05-14 16:28:16 -0300

    main

--- src/componentes/Header/index.tsx
+++ src/componentes/Header/index.tsx
@@ -1,3 +1,21 @@
+
 export const Header = () => {
+
+
+    return (
+        <View style={styles.container}>
+          <Text>GRANCOFFEE Preventivas!</Text>
+          <Text>Pichek Developer</Text>
+          <StatusBar style="auto" />
+        </View>
+      );
+    }
     
-}
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    });
--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -1,21 +1,10 @@
 import { View, Text, StyleSheet } from "react-native"
-import { StatusBar } from "expo-status-bar";
+import { Header } from "react-native/Libraries/NewAppScreen";
 
 export default function Home(){
     return (
-      <View style={styles.container}>
-        <Text>GRANCOFFEE Preventivas!</Text>
-        <Text>Pichek Developer</Text>
-        <StatusBar style="auto" />
+      <View>
+        <Header/>
       </View>
     );
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+}
